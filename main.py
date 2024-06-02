172fd284aaed9e7032e6349903dc840433f00268
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-06-02 13:18:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from ultralytics import YOLO
 import yolov8
-import ultralytics.utils
 
 #Importando MODELO
 model = YOLO('best.pt')
