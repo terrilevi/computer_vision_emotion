bddaaf2a3d02e386d079cf7bd854571ceb863ffd
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-06-02 14:48:06 -0500

    Agregando select box para seleccionar el modelo .pt

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import cv2
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import ultralytics
 from ultralytics import YOLO
-#import yolov8
 
 #Importando MODELO
 model = YOLO('best.pt')
+model2 = YOLO('last.pt')
 
 tab1, tab2 = st.tabs(["Haz un  dibujito ✍️", "Sube una imágen ⬆️"])
 
 with tab1:
     import streamlit as st
+
+
     st.header("Haz un dibujito")
 
     # Especifica los parametros canvas de la aplicacion canvas
@@ -30,6 +31,9 @@ with tab1:
 
     realtime_update = st.checkbox("Actualizar en tiempo real", True)
 
+    modelos_disponibles = {'Modelo 1 (best.pt)': model, 'Modelo 2 (last.pt)': model2}
+    option = st.selectbox("Escoja modelo: ", list(modelos_disponibles.keys()))
+
     # Create a canvas component
     canvas_result = st_canvas(
         fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
@@ -42,19 +46,22 @@ with tab1:
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="canvas",
     )
+    
 
+    # Boton predecir:
 
-
-    # Add a Predict button
     if st.button("Predecir dibujo"):
         if canvas_result.image_data is not None:
-            st.write("Imagen procesada")
-            #st.image(canvas_result.image_data)
             # Convertir la imagen a formato RGB
             image = Image.fromarray(canvas_result.image_data.astype('uint8'), 'RGBA')
             image = image.convert('RGB')
+            
+            # Obtener el modelo seleccionado
+            selected_model = modelos_disponibles[option]
+            
             # Realizar predicción en el dibujo
-            results = model(image)
+            results = selected_model(image)
+            
             # Mostrar los resultados de la predicción
             im = results[0].plot()
             st.image(im)
@@ -62,21 +69,32 @@ with tab1:
             st.write("No hay imagen")
 
 with tab2:
+
     import streamlit as st
+
     st.header("Sube una imágen")
     source_img = st.file_uploader(
     "Escoge una imágen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    
+    modelos_subidos = {'Modelo 1 (best.pt)': model, 'Modelo 2 (last.pt)': model2}
+    option = st.selectbox("Escoger modelo: ", list(modelos_subidos.keys()))
+
+
     if st.button("Predecir imagen"):
         if source_img is not None:
-            st.write("Imagen procesada")
-            #st.image(source_img)
+
             # Convertir la imagen a formato RGB
             image = Image.open(source_img)
             image = image.convert('RGB')
+
+            modelo_seleccionado = modelos_subidos[option]
+
             # Realizar predicción en la imagen subida
-            results = model(image)
+            results = modelo_seleccionado(image)
+            
             # Mostrar los resultados de la predicción
             im = results[0].plot()
             st.image(im)
+
         else:
             st.write("No hay imagen")
